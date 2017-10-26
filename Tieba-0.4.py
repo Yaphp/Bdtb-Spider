4b32eae64eb4cd0c21cb8116ea3c5a2de88657c9
Yaphp <33092515+Yaphp@users.noreply.github.com>
2017-10-26 09:22:01 +0800

    Update Tieba-0.4.py

--- Tieba-0.4.py
+++ Tieba-0.4.py
@@ -11,34 +11,6 @@ import threading
 
 
 
-#处理页面标签类
-class Tool:
-    #去除img标签,7位长空格
-    removeImg = re.compile('<img.*?>| {7}|')
-    #删除超链接标签
-    removeAddr = re.compile('<a.*?>|</a>')
-    #把换行的标签换为\n
-    replaceLine = re.compile('<tr>|<div>|</div>|</p>')
-    #将表格制表<td>替换为\t
-    replaceTD= re.compile('<td>')
-    #把段落开头换为\n加空两格
-    replacePara = re.compile('<p.*?>')
-    #将换行符或双换行符替换为\n
-    replaceBR = re.compile('<br><br>|<br>')
-    #将其余标签剔除
-    removeExtraTag = re.compile('<.*?>')
-    def replace(self,x):
-        x = re.sub(self.removeImg,"",x)
-        x = re.sub(self.removeAddr,"",x)
-        x = re.sub(self.replaceLine,"\n",x)
-        x = re.sub(self.replaceTD,"\t",x)
-        x = re.sub(self.replacePara,"\n    ",x)
-        x = re.sub(self.replaceBR,"\n",x)
-        x = re.sub(self.removeExtraTag,"",x)
-        #strip()将前后多余内容删除
-        return x.strip()
-
-
 
 class Tieba:
     def __init__(self,url):
@@ -197,7 +169,7 @@ print("|-------------------开始执行---------------------------->")
 # input("抓取完成,按任意键退出：")
 #Tieba(url).getPage()
 
-print("输入tieba的地址进行爬取,例如：http://tieba.baidu.com/f?kw=苍溪实验中学")
+print("输入tieba的地址进行爬取,例如：http://tieba.baidu.com/f?kw=实验中学")
 tiebaurl=str(input("请输入："))
 url=tiebaurl+'&ie=utf-8&pn='
 Tieba(url).start_go()
